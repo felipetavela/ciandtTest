ea8b7e593777faab2de103659c965af5e2c3bbae
Felipe Tavela <felipe.tavela@cpb.com.br>
2025-09-24 14:32:03 -0300

    Initial Commit

---
+++ CiandtTest/CiandtTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CiandtTestApp.swift
+//  CiandtTest
+//
+//  Created by Felipe Tavela on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CiandtTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
